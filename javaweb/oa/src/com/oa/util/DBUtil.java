1b8a5e2aa10c58f8bb4ee8858fede5918834bac7
icestream32 <1601334224@qq.com>
2022-03-06 13:59:01 +0800

    oa项目的项目原型搭建和数据库工具类的创建

---
+++ javaweb/oa/src/com/oa/util/DBUtil.java
@@ -0,0 +1,68 @@
+package com.oa.util;
+
+import java.sql.*;
+import java.util.ResourceBundle;
+
+/**
+ * 数据库连接工具类
+ */
+public class DBUtil {
+
+    // 获取资源绑定器
+    private static final ResourceBundle bundle = ResourceBundle.getBundle("/resource/jdbc");
+
+    private static final String driver = bundle.getString("driver");
+    private static final String url = bundle.getString("url");
+    private static final String username = bundle.getString("user");
+    private static final String password = bundle.getString("password");
+
+    /*
+      注册驱动
+     */
+    static {
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取数据库连接对象
+     * @return 数据库连接对象
+     * @throws SQLException 连接异常
+     */
+    private static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url,username,password);
+    }
+
+    /**
+     * 释放资源
+     * @param conn 数据库连接对象
+     * @param stmt 数据库操作对象
+     * @param rs 处理结果集
+     */
+    private static void close(Connection conn, Statement stmt, ResultSet rs) {
+        if (rs != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
